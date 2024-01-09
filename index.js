0492eb477dbeb0cda00e4d3b30c3b835e020ceca
Ylia89 <154316061+Ylia89@users.noreply.github.com>
2024-01-09 16:02:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-function getInfo(){
-    $('button').click(function(){
-        let info = $(this).value()
-        sessionStorage.setItem('get-info', info.value)
-        return true
-    })
+function saveInfo(){
+    let userInput = document.getElementById('uname').value
+    let pswInput = document.getElementById('psw').value
+    console.log(userInput, pswInput)
+    localStorage.setItem('login',JSON.stringify({user:userInput, psw:pswInput}))
 }
-document.addEventListener('DOMContentLoaded', function(){
-    getInfo()
-})
 
-function addText(){
-    document.getElementById('uname').innerHTML = sessionStorage.getItem('get-text')
+
+document.getElementById('btn').addEventListener('click', saveInfo)
+let localData = JSON.parse(localStorage.getItem('login'))
+if(localData!=null){
+    document.getElementById('user').textContent = localData.user
+    document.getElementById('password').textContent = localData.psw
 }
-document.addEventListener('DOMContentLoaded', function(){
-    addText
-})
+
 
